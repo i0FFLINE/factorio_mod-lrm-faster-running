cfd2a39b3a17b1ecc9e335b150ee5100eeb113d7
iOFFLINE <me@ioffline.ru>
2025-10-28 17:50:54 +0300

    bugfix

--- settings.lua
+++ settings.lua
@@ -3,7 +3,7 @@ data:extend{
     type = "double-setting",
     name = "lrm-faster-running-multiplier",
     setting_type = "runtime-global",
-    default_value = 1.0,
+    default_value = 2.0,
     minimum_value = 0.1,
     maximum_value = 10.0,
     allowed_values = {},
